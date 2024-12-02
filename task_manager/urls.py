c94390b57cc14b3a928e5e4ec509b56f221fb434
anirudh-248 <anirudhphaniraj1@gmail.com>
2024-12-02 22:59:17 +0530

    first commit

---
+++ task_manager/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('add_task', views.add_task, name='add_task'),
+    path('toggle_task_status', views.toggle_task_status, name='toggle_task_status'),
+]
---
+++ task_manager/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from .models import Task
+
+# Create your views here.
+def index(request):
+    tasks = Task.objects.filter(completed=False)
+    completed_tasks = Task.objects.filter(completed=True)
+    return render(request, 'index.html', {'tasks': tasks, 'completed_tasks': completed_tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        task = request.POST['task']
+        Task.objects.create(task=task)
+        return redirect('index')
+    
+def toggle_task_status(request):
+    if request.method == 'POST':
+        task_id = request.POST.get('task_id')
+        task = Task.objects.get(id=task_id)
+        task.completed = not task.completed
+        task.save()
+    return redirect('index')
