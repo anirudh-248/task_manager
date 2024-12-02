fb942844f816c6b634f5ff6db52682077dafd4fb
anirudh-248 <anirudhphaniraj1@gmail.com>
2024-12-03 00:21:46 +0530

    updated wsgi file

--- mini_project/wsgi.py
+++ mini_project/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 settings_module = "mini_project.deployment" if 'WEBSITE_HOSTNAME' in os.environ else "mini_project.settings"
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mini_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 application = get_wsgi_application()
