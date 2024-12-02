99d05d2cfbf38831173fd0706e36bc016b067f9a
anirudh-248 <anirudhphaniraj1@gmail.com>
2024-12-02 23:23:26 +0530

    added code for deployment

--- mini_project/wsgi.py
+++ mini_project/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+settings_module = "mini_project.deployment" if 'WEBSITE_HOSTNAME' in os.environ else "mini_project.settings"
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mini_project.settings")
 
 application = get_wsgi_application()
