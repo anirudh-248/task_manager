4a37ed5f3983d81e92f454cee76486c4a17a3667
anirudh-248 <anirudhphaniraj1@gmail.com>
2024-12-03 22:45:14 +0530

    modified deployment file

--- mini_project/deployment.py
+++ mini_project/deployment.py
@@ -4,7 +4,7 @@ from .settings import BASE_DIR
 
 SECRET_KEY = os.environ['SECRET']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
-CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = True
 
 MIDDLEWARE = [
@@ -21,7 +21,7 @@ MIDDLEWARE = [
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+connection_string = os.environ["AZURE_POSTGRESQL_CONNECTIONSTRING"]
 parameters = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split(' ')}
 
 DATABASES = {
@@ -31,5 +31,6 @@ DATABASES = {
         "USER": parameters['user'],
         "PASSWORD": parameters['password'],
         "HOST": parameters['host'],
+        "PORT": parameters['port'],
     }
 }
