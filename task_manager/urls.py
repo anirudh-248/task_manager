d6a8bc40c37c381f24f084322db94f78eb2343be
anirudh-248 <anirudhphaniraj1@gmail.com>
2024-12-09 22:34:04 +0530

    added authentication and division of tasks by users

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+    path('register', views.register, name='register'),
+    path('manager', views.manager, name='manager'),
     path('add_task', views.add_task, name='add_task'),
     path('toggle_task_status', views.toggle_task_status, name='toggle_task_status'),
 ]
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,18 +1,62 @@
 from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib import messages
+from django.contrib.auth.models import User
 from .models import Task
 
 # Create your views here.
+
 def index(request):
-    has_tasks = Task.objects.filter(completed=False).exists()
-    tasks = Task.objects.filter(completed=False)
-    completed_tasks = Task.objects.filter(completed=True)
-    return render(request, 'index.html', {'tasks': tasks, 'completed_tasks': completed_tasks, 'has_tasks': has_tasks})
+    return render(request, 'index.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('manager')
+        else:
+            messages.info(request, 'Invalid email or password')
+            return redirect('login')
+    return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        if password != password2:
+            messages.info(request, 'Passwords do not match')
+            return redirect('register')
+        else:
+            user = User.objects.create_user(username=username, email=email, password=password)
+            if user:
+                messages.success(request, 'Account created successfully')
+                return redirect('login')
+            else:
+                messages.info(request, 'An error occurred while creating your account')
+                return redirect('register')
+    return render(request, 'register.html')
+
+def manager(request):
+    has_tasks = Task.objects.filter(user=request.user , completed=False).exists()
+    tasks = Task.objects.filter(user=request.user, completed=False)
+    completed_tasks = Task.objects.filter(user=request.user, completed=True)
+    return render(request, 'manager.html', {'tasks': tasks, 'completed_tasks': completed_tasks, 'has_tasks': has_tasks})
 
 def add_task(request):
     if request.method == 'POST':
         task = request.POST['task']
-        Task.objects.create(task=task)
-        return redirect('index')
+        user = request.user
+        Task.objects.create(task=task, user=user)
+        return redirect('manager')
     
 def toggle_task_status(request):
     if request.method == 'POST':
@@ -20,4 +64,4 @@ def toggle_task_status(request):
         task = Task.objects.get(id=task_id)
         task.completed = not task.completed
         task.save()
-    return redirect('index')
+    return redirect('manager')
